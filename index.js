f22a96b4326fe71c7f077c73ab5ead2789159c6f
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-02-17 12:03:03 +0800

    Use postcss

--- index.js
+++ index.js
@@ -4,12 +4,13 @@ var assign = require('object-assign')
 module.exports = function (options) {
   if (!options) options = {}
   options.sass = assign({}, sassDefaults(), options.sass || {})
+  options.postcss = assign({}, postcssDefaults(), options.postcss || {})
 
   return function (files, ms, done) {
     ware()
       .use(require('metalsmith-ignore')([ '**/_*.{scss,sass}' ]))
       .use(require('metalsmith-sass')(options.sass))
-      .use(require('metalsmith-autoprefixer')(options.autoprefixer))
+      .use(require('metalsmith-postcss')(toPostcss(options.postcss)))
       .run(files, ms, done)
   }
 }
@@ -31,4 +32,29 @@ function sassDefaults () {
       sourceMap: false
     }
   }
+}
+
+/*
+ * Private: returns an array of postcss plugins.
+ *
+ *     toPostcss({
+ *       'postcss-nested': {},
+ *       'postcss-pseudoelements': {}
+ *     })
+ *
+ *     // same as:
+ *     [
+ *       require('postcss-nested')({}),
+ *       require('postcss-pseudoelements')({})
+ *     ]
+ */
+
+function toPostcss (plugins) {
+  return Object.keys(plugins).map(function (plugin) {
+    return require(plugin)(plugins[plugin])
+  })
+}
+
+function postcssDefaults () {
+  return { 'postcss-cssnext': {} }
 }
